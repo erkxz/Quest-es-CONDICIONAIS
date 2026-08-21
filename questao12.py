a3d48e98565b1ad5753b824cbc169fd1709f9247
erkxz <erigufran@gmail.com>
2026-08-20 23:05:54 -0300

    Update questao12.py

--- questao12.py
+++ questao12.py
@@ -1,6 +1,5 @@
 salario = float(input("digite o salario: "))
 prestacao = float(input("digite o valor da prestacao: "))
-
 if prestacao > salario * 0.20:
     print("emprestimo nao concebido")
 else:
