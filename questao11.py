fcbb83bb969aaa36cdb3342476d431d038e4c8ce
erkxz <erigufran@gmail.com>
2026-08-20 23:03:24 -0300

    Update questao11.py
    Updated input prompts to lowercase.

--- questao11.py
+++ questao11.py
@@ -1,5 +1,5 @@
-n1 = float(input("Digite o primeiro numero: "))
-n2 = float(input("Digite o segundo numero: "))
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
 soma = n1 + n2
 
 if soma > 20:
