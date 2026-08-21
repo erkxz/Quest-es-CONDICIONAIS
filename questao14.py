71f13d2181c9a56b5a499d47baae499eade9cfba
erkxz <erigufran@gmail.com>
2026-08-20 23:04:17 -0300

    Update questao14.py

--- questao14.py
+++ questao14.py
@@ -1,6 +1,6 @@
-n1 = float(input("Digite o primeiro numero: "))
-n2 = float(input("Digite o segundo numero: "))
-n3 = float(input("Digite o terceiro numero: "))
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
+n3 = float(input("digite o terceiro numero: "))
 
 lista = [n1, n2, n3]
 lista.sort()
