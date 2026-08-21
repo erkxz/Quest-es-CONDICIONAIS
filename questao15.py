9bfbda26a86478417a8c71f938d3bcba4da9cb64
erkxz <erigufran@gmail.com>
2026-08-20 23:05:39 -0300

    Update questao15.py

--- questao15.py
+++ questao15.py
@@ -1,9 +1,7 @@
-altura = float(input("Digite a altura (m): "))
-sexo = input("Digite o sexo (M/F): ").upper()
-
+altura = float(input("digite a altura (m): "))
+sexo = input("digite o sexo (M/F): ").upper()
 if sexo == "M":
     peso = (72.7 * altura) - 58
 else:
     peso = (62.1 * altura) - 47
-
 print("Peso ideal:", peso)
