95f795757ef18b1956b101ef4a1fc97fea2e5b5c
erkxz <erigufran@gmail.com>
2026-08-20 23:01:09 -0300

    Update questao03.py

--- questao03.py
+++ questao03.py
@@ -1,5 +1,5 @@
-n1 = float(input("Digite o primeiro numero: "))
-n2 = float(input("Digite o segundo numero: "))
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
 if n1 < n2:
     print(n1, n2)
 else:
