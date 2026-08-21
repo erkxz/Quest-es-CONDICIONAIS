6f9965e729081e0f4f954fe83fa22ebb5107ab1d
erkxz <erigufran@gmail.com>
2026-08-20 23:02:22 -0300

    Update questao06.py

--- questao06.py
+++ questao06.py
@@ -1,4 +1,4 @@
-nome = input("Digite o nome: ")
-idade = int(input("Digite a idade: "))
+nome = input("digite o nome: ")
+idade = int(input("digite a idade: "))
 if idade < 25:
     print(nome, "- Aceita")
