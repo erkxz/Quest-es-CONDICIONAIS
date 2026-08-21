08b359f1fb1d9495c4169811a456bf2e7107ca50
erkxz <erigufran@gmail.com>
2026-08-20 23:03:06 -0300

    Change input prompts to lowercase
    Updated input prompts to lowercase for consistency.

--- questao10.py
+++ questao10.py
@@ -1,7 +1,7 @@
 import math
 
-n1 = float(input("Digite o primeiro numero: "))
-n2 = float(input("Digite o segundo numero: "))
+n1 = float(input("digite o primeiro numero: "))
+n2 = float(input("digite o segundo numero: "))
 
 if n1 == n2:
     print("cubo:", n1 ** 3, n2 ** 3)
