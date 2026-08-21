63726b24d325400674566e4b4129269d1a5c6dd0
erkxz <erigufran@gmail.com>
2026-08-20 23:06:46 -0300

    Update questao16.py

--- questao16.py
+++ questao16.py
@@ -1,9 +1,7 @@
-valor_compra = float(input("Digite o valor de compra: "))
-
+valor_compra = float(input("digite o valor de compra: "))
 if valor_compra < 20:
     lucro = 0.45
 else:
     lucro = 0.30
-
 valor_venda = valor_compra + (valor_compra * lucro)
 print("Valor de venda:", valor_venda)
