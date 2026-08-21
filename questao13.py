4c1b4322b6e02dca61aaf2042c2542e2936aae72
erkxz <erigufran@gmail.com>
2026-08-20 23:04:06 -0300

    Update questao13.py

--- questao13.py
+++ questao13.py
@@ -1,8 +1,8 @@
-valor = int(input("Digite um numero inteiro: "))
+valor = int(input("digite um numero inteiro: "))
 div3 = valor % 3 == 0
 div5 = valor % 5 == 0
 
 if (div3 or div5) and not (div3 and div5):
-    print("Divisivel por 3 ou 5, mas nao pelos dois")
+    print("divisivel por 3 ou 5, mas nao pelos dois")
 else:
-    print("Nao atende a condicao")
+    print("nao atende a condicao")
