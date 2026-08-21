03524df8a53cab8c11af2bd884db2f338fb15758
erkxz <erigufran@gmail.com>
2026-08-20 23:01:56 -0300

    Change input prompts to lowercase
    Updated input prompts to lowercase for consistency.

--- questao04.py
+++ questao04.py
@@ -1,6 +1,6 @@
-a = int(input("Digite o valor de A: "))
-b = int(input("Digite o valor de B: "))
-c = int(input("Digite o valor de C: "))
+a = int(input("digite o valor de A: "))
+b = int(input("digite o valor de B: "))
+c = int(input("digite o valor de C: "))
 soma = a + b + c
 if soma >= 100:
     print(soma)
